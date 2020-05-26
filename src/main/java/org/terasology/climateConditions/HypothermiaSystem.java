07534b98440a6059d7e8e8ed55e4f02c6e981e89
ktksan <kartikeya.sanwal@gmail.com>
2020-05-26 18:06:40 +0530

    Change Speed with GetMaxSpeed and AffectJumpForce

--- src/main/java/org/terasology/climateConditions/HypothermiaSystem.java
+++ src/main/java/org/terasology/climateConditions/HypothermiaSystem.java
@@ -15,13 +15,17 @@
  */
 package org.terasology.climateConditions;
 
+import org.terasology.alterationEffects.speed.*;
 import org.terasology.biomesAPI.OnBiomeChangedEvent;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.logic.characters.AffectJumpForceEvent;
 import org.terasology.logic.characters.CharacterMovementComponent;
+import org.terasology.logic.characters.GetMaxSpeedEvent;
+import org.terasology.logic.characters.MovementMode;
 import org.terasology.logic.location.LocationComponent;
 import org.terasology.logic.players.PlayerCharacterComponent;
 import org.terasology.physics.events.MovedEvent;
@@ -29,6 +33,8 @@ import org.terasology.physics.events.MovedEvent;
 @RegisterSystem(value = RegisterMode.AUTHORITY)
 public class HypothermiaSystem extends BaseComponentSystem {
     private float thresholdHeight = 60f;
+    private float WalkSpeedMultiplier = 0.6f;
+    private float JumpSpeedMultiplier = 0.7f;
 
     @ReceiveEvent(components = {PlayerCharacterComponent.class, CharacterMovementComponent.class})
     public void addHypothermia(MovedEvent event, EntityRef player, LocationComponent location, CharacterMovementComponent movement) {
@@ -65,4 +71,20 @@ public class HypothermiaSystem extends BaseComponentSystem {
 
         }
     }
+
+    @ReceiveEvent
+    public void modifySpeed(GetMaxSpeedEvent event, EntityRef player) {
+        // If the entity is walking and they have a walk speed effect, boost their walking speed by the multiplier.
+        if (event.getMovementMode() == MovementMode.WALKING && player.hasComponent(HypothermiaComponent.class)) {
+            event.multiply(WalkSpeedMultiplier);
+        }
+    }
+
+    @ReceiveEvent
+    public void modifyJumpSpeed(AffectJumpForceEvent event, EntityRef player) {
+        if (player.hasComponent(HypothermiaComponent.class)) {
+            event.multiply(JumpSpeedMultiplier);
+        }
+    }
+
 }
