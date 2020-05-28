a56c27edeb28dfdc36e7af719b0b0a70714c3c58
ktksan <50916693+ktksan@users.noreply.github.com>
2020-05-28 17:10:49 +0530

    feat: Slow down player with Hypothermia (#11)
    Slow down player by reacting to GetMaxSpeed and AffectJumpForce events.

--- src/main/java/org/terasology/climateConditions/HypothermiaSystem.java
+++ src/main/java/org/terasology/climateConditions/HypothermiaSystem.java
@@ -20,27 +20,43 @@ import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.logic.characters.AffectJumpForceEvent;
 import org.terasology.logic.characters.CharacterMovementComponent;
-import org.terasology.logic.location.LocationComponent;
+import org.terasology.logic.characters.GetMaxSpeedEvent;
+import org.terasology.logic.characters.MovementMode;
 import org.terasology.logic.players.PlayerCharacterComponent;
 import org.terasology.physics.events.MovedEvent;
 
 @RegisterSystem(value = RegisterMode.AUTHORITY)
 public class HypothermiaSystem extends BaseComponentSystem {
     private float thresholdHeight = 60f;
+    private float walkSpeedMultiplier = 0.6f;
+    private float jumpSpeedMultiplier = 0.7f;
 
     @ReceiveEvent(components = {PlayerCharacterComponent.class, CharacterMovementComponent.class})
-    public void observeDangerZone(MovedEvent event, EntityRef player, LocationComponent location, CharacterMovementComponent movement) {
+    public void observeDangerZone(MovedEvent event, EntityRef player) {
         //TODO: react on OnBiomeChangedEvent to handle the danger zone
         float height = event.getPosition().getY();
         float lastHeight = height - event.getDelta().getY();
         if (height > thresholdHeight && lastHeight <= thresholdHeight) {
-                player.addOrSaveComponent(new HypothermiaComponent());
+            player.addOrSaveComponent(new HypothermiaComponent());
         }
         if (height < thresholdHeight && lastHeight >= thresholdHeight) {
-            if(player.hasComponent(HypothermiaComponent.class)){
+            if (player.hasComponent(HypothermiaComponent.class)) {
                 player.removeComponent(HypothermiaComponent.class);
             }
         }
+    }
+
+    @ReceiveEvent(components = {HypothermiaComponent.class})
+    public void modifySpeed(GetMaxSpeedEvent event, EntityRef player) {
+        if (event.getMovementMode() == MovementMode.WALKING) {
+            event.multiply(walkSpeedMultiplier);
+        }
+    }
+
+    @ReceiveEvent(components = {HypothermiaComponent.class})
+    public void modifyJumpSpeed(AffectJumpForceEvent event, EntityRef player) {
+        event.multiply(jumpSpeedMultiplier);
     }
 }
