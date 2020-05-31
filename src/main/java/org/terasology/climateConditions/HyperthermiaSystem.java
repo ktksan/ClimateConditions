9a4701a33e0a7aa69794fe0f7c0c0f9dc6a0b3f5
ktksan <kartikeya.sanwal@gmail.com>
2020-05-31 19:30:42 +0530

    1

--- src/main/java/org/terasology/climateConditions/HyperthermiaSystem.java
+++ src/main/java/org/terasology/climateConditions/HyperthermiaSystem.java
@@ -17,33 +17,38 @@ package org.terasology.climateConditions;
 
 import org.terasology.biomesAPI.OnBiomeChangedEvent;
 import org.terasology.entitySystem.entity.EntityRef;
-import org.terasology.entitySystem.entity.lifecycleEvents.OnAddedComponent;
 import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.logic.characters.AffectJumpForceEvent;
 import org.terasology.logic.characters.GetMaxSpeedEvent;
+import org.terasology.logic.characters.MovementMode;
 import org.terasology.thirst.component.ThirstComponent;
 
 @RegisterSystem(value = RegisterMode.AUTHORITY)
 public class HyperthermiaSystem extends BaseComponentSystem {
+    private float walkSpeedMultiplier = 0.6f;
+    private float jumpSpeedMultiplier = 0.7f;
 
     @ReceiveEvent
-    public void DangerZone(OnBiomeChangedEvent event, EntityRef player, ThirstComponent thirst) {
-        //TODO: react on OnBiomeChangedEvent to handle the danger zone
-        String Name2 = "a";
-        if (event.getNewBiome().getDisplayName().equals("DESERT")) {
-            player.addOrSaveComponent(new HypothermiaComponent());
-            String Name = "a";
+    public void modifySpeed2(GetMaxSpeedEvent event, EntityRef player, ThirstComponent thirst) {
+        if (event.getMovementMode() == MovementMode.WALKING) {
+            event.multiply(1.2f);
+            thirst.normalDecayPerSecond = 3f;
         }
     }
 
-    @ReceiveEvent
-    public void onWalk(GetMaxSpeedEvent event, EntityRef player) {
-        String Name3 = "a";
 
-        //   delayManager.addPeriodicAction(player, VisibleBreathingSystem.VISIBLE_BREATH_ACTION_ID, initialDelay,
-        //   breathInterval);
+    @ReceiveEvent
+    public void onBiomeChange(OnBiomeChangedEvent event, EntityRef player, ThirstComponent thirst) {
+            String a = "A";
+            thirst.normalDecayPerSecond = 3f;
+
     }
 
+    @ReceiveEvent
+    public void modifyJumpSpeed2(AffectJumpForceEvent event, EntityRef player) {
+        event.multiply(0.8f);
+    }
 }
--- src/main/java/org/terasology/climateConditions/HypothermiaSystem.java
+++ src/main/java/org/terasology/climateConditions/HypothermiaSystem.java
@@ -29,7 +29,7 @@ import org.terasology.physics.events.MovedEvent;
 
 @RegisterSystem(value = RegisterMode.AUTHORITY)
 public class HypothermiaSystem extends BaseComponentSystem {
-    private float thresholdHeight = 60f;
+    private float thresholdHeight = 6000f;
     private float walkSpeedMultiplier = 0.6f;
     private float jumpSpeedMultiplier = 0.7f;
 
