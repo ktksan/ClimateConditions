a750a75e3118a810529ed2f2147dc2fae8a21f62
ktksan <kartikeya.sanwal@gmail.com>
2020-05-31 16:15:09 +0530

    DesertTrial

--- src/main/java/org/terasology/climateConditions/TrialSystem.java
+++ src/main/java/org/terasology/climateConditions/TrialSystem.java
@@ -15,35 +15,33 @@
  */
 package org.terasology.climateConditions;
 
-import org.terasology.biomesAPI.OnBiomeChangedEvent;
 import org.terasology.entitySystem.entity.EntityRef;
-import org.terasology.entitySystem.entity.lifecycleEvents.OnAddedComponent;
 import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.logic.characters.AffectJumpForceEvent;
+import org.terasology.logic.characters.CharacterMovementComponent;
 import org.terasology.logic.characters.GetMaxSpeedEvent;
-import org.terasology.thirst.component.ThirstComponent;
+import org.terasology.logic.characters.MovementMode;
+import org.terasology.logic.players.PlayerCharacterComponent;
+import org.terasology.physics.events.MovedEvent;
 
 @RegisterSystem(value = RegisterMode.AUTHORITY)
-public class HyperthermiaSystem extends BaseComponentSystem {
+public class TrialSystem extends BaseComponentSystem {
+    //private float thresholdHeight = 60f;
+    private float walkSpeedMultiplier = 0.6f;
+    private float jumpSpeedMultiplier = 0.7f;
 
     @ReceiveEvent
-    public void DangerZone(OnBiomeChangedEvent event, EntityRef player, ThirstComponent thirst) {
-        //TODO: react on OnBiomeChangedEvent to handle the danger zone
-        String Name2 = "a";
-        if (event.getNewBiome().getDisplayName().equals("DESERT")) {
-            player.addOrSaveComponent(new HypothermiaComponent());
-            String Name = "a";
+    public void modifySpeed(GetMaxSpeedEvent event, EntityRef player) {
+        if (event.getMovementMode() == MovementMode.WALKING) {
+            event.multiply(4f);
         }
     }
 
     @ReceiveEvent
-    public void onWalk(GetMaxSpeedEvent event, EntityRef player) {
-        String Name3 = "a";
-
-        //   delayManager.addPeriodicAction(player, VisibleBreathingSystem.VISIBLE_BREATH_ACTION_ID, initialDelay,
-        //   breathInterval);
+    public void modifyJumpSpeed(AffectJumpForceEvent event, EntityRef player) {
+        event.multiply(jumpSpeedMultiplier);
     }
-
 }
