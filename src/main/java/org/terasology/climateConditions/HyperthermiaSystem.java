519d72589c91fd99e3aca3b6eeb26438f915e6dd
ktksan <kartikeya.sanwal@gmail.com>
2020-06-01 22:24:26 +0530

    Add Hyperthermia Component System

--- src/main/java/org/terasology/climateConditions/HyperthermiaSystem.java
+++ src/main/java/org/terasology/climateConditions/HyperthermiaSystem.java
@@ -15,31 +15,50 @@
  */
 package org.terasology.climateConditions;
 
+import org.terasology.biomesAPI.Biome;
+import org.terasology.biomesAPI.BiomeManager;
 import org.terasology.biomesAPI.OnBiomeChangedEvent;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
-import org.terasology.logic.characters.AffectJumpForceEvent;
+import org.terasology.logic.characters.CharacterMovementComponent;
 import org.terasology.logic.characters.GetMaxSpeedEvent;
-import org.terasology.logic.characters.MovementMode;
+import org.terasology.logic.location.LocationComponent;
+import org.terasology.logic.players.PlayerCharacterComponent;
+import org.terasology.logic.players.event.OnPlayerSpawnedEvent;
+import org.terasology.math.geom.Vector3i;
 import org.terasology.thirst.component.ThirstComponent;
+
+import java.util.Optional;
 
 @RegisterSystem(value = RegisterMode.AUTHORITY)
 public class HyperthermiaSystem extends BaseComponentSystem {
+    private float walkSpeedMultiplier = 0.7f;
+    BiomeManager biomeManager;
 
-    @ReceiveEvent
+    @ReceiveEvent(components = {PlayerCharacterComponent.class, CharacterMovementComponent.class})
     public void onBiomeChange(OnBiomeChangedEvent event, EntityRef player, ThirstComponent thirst) {
-            if(event.getNewBiome().getDisplayName().equals("DESERT")) {
-                thirst.normalDecayPerSecond = 3f;
-                player.addOrSaveComponent(new HyperthermiaComponent());
-                String A = "a";
+        if (event.getNewBiome().getDisplayName().equals("Desert")) {
+            player.addOrSaveComponent(new HyperthermiaComponent());
+        } else {
+            if (player.hasComponent(HyperthermiaComponent.class)) {
+                player.removeComponent(HyperthermiaComponent.class);
             }
+        }
     }
 
-    @ReceiveEvent
-    public void modifySpeed(GetMaxSpeedEvent event, EntityRef player){
-        event.multiply(4f);
+    @ReceiveEvent(components = {HyperthermiaComponent.class})
+    public void modifySpeed(GetMaxSpeedEvent event, EntityRef player) {
+        event.multiply(walkSpeedMultiplier);
+    }
+
+    @ReceiveEvent(components = {PlayerCharacterComponent.class, CharacterMovementComponent.class})
+    public void onSpawn(OnPlayerSpawnedEvent event, EntityRef player, LocationComponent location) {
+        final Optional<Biome> biome = biomeManager.getBiome(new Vector3i(location.getLocalPosition()));
+        if (biome.get().getDisplayName().equals("Desert")) {
+            player.addOrSaveComponent(new HyperthermiaComponent());
+        }
     }
 }
