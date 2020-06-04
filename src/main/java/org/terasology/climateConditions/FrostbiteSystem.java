110512978e521f9a549edb62ef37861d6e432c37
ktksan <50916693+ktksan@users.noreply.github.com>
2020-06-04 15:15:48 +0530

    2
    Co-authored-by: jdrueckert <jd.rueckert@googlemail.com>

--- src/main/java/org/terasology/climateConditions/FrostbiteSystem.java
+++ src/main/java/org/terasology/climateConditions/FrostbiteSystem.java
@@ -37,8 +37,9 @@ import org.terasology.utilities.random.FastRandom;
 import org.terasology.utilities.random.Random;
 
 /**
- * This system is responsible for periodic frostbite damage (along with stun and a sound effect) to the player as long
- * as the player has a Hypothermia component.
+ * Adds frostbite to the player.
+ * Frostbite is a periodic effect that, e.g., damages and stuns the player.
+ * Is only active iff the player has a {@link HypothermiaComponent}.
  */
 @RegisterSystem(RegisterMode.AUTHORITY)
 public class FrostbiteSystem extends BaseComponentSystem {
