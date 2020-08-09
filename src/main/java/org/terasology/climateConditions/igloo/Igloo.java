4e96da1b823ff168b81a068f4223f8b077e8d1d9
ktksan <kartikeya.sanwal@gmail.com>
2020-08-10 01:32:54 +0530

    Add note on SIZE attribute in igloo

--- src/main/java/org/terasology/climateConditions/igloo/Igloo.java
+++ src/main/java/org/terasology/climateConditions/igloo/Igloo.java
@@ -8,5 +8,11 @@ package org.terasology.climateConditions.igloo;
  * They are very rare and represent remaining traces of abandoned settlements in the Snow Biome.
  */
 public class Igloo {
+    /**
+     * Used by {@link IglooProvider} to reserve borders for the Igloo Facet.
+     * NOTE: The SIZE attribute has directly been obtained by measuring the extents of the Igloo Structure Template
+     * in-game and must be carefully measured for every structure template. Not doing so may result in improper
+     * formation of the structure or may even lead to loads of NPE/ Out of Bounds exceptions during world generation.
+     */
     public static final int SIZE = 13;
 }
